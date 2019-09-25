d77f0f7bc964e23ca103f27638396f9d34ec8079
diana lopez <dianalopez5cbba@gmail.bo>
2019-09-25 17:59:54 -0400

    tarea

--- main.js
+++ main.js
@@ -5,6 +5,18 @@ var Bar = /** @class */ (function () {
         this.nombre = nom;
         this.capacidad = cap;
     }
+    Bar.prototype.setNombre = function (nom) {
+        this.nombre = nom;
+    };
+    Bar.prototype.setCapacidad = function (cap) {
+        this.capacidad = cap;
+    };
+    Bar.prototype.getNombre = function () {
+        return this.nombre;
+    };
+    Bar.prototype.getCapacidad = function () {
+        return this.capacidad;
+    };
     return Bar;
 }());
 exports.Bar = Bar;
@@ -13,6 +25,18 @@ var BarMan = /** @class */ (function () {
         this.nombre = nom;
         this.apellido = ape;
     }
+    BarMan.prototype.setNombre = function (nom) {
+        this.nombre = nom;
+    };
+    BarMan.prototype.setApellido = function (ape) {
+        this.apellido = ape;
+    };
+    BarMan.prototype.getNombre = function () {
+        return this.nombre;
+    };
+    BarMan.prototype.getApellido = function () {
+        return this.apellido;
+    };
     return BarMan;
 }());
 exports.BarMan = BarMan;
@@ -22,18 +46,44 @@ var Trago = /** @class */ (function () {
         this.color = col;
         this.cantidad = cant;
     }
+    Trago.prototype.setNombre = function (nom) {
+        this.nombre = nom;
+    };
+    Trago.prototype.setColor = function (col) {
+        this.color = col;
+    };
+    Trago.prototype.setcantidad = function (cant) {
+        this.cantidad = cant;
+    };
+    Trago.prototype.getNombre = function () {
+        return this.nombre;
+    };
+    Trago.prototype.getColor = function () {
+        return this.color;
+    };
+    Trago.prototype.getCantidad = function () {
+        return this.cantidad;
+    };
     return Trago;
 }());
 exports.Trago = Trago;
 var Bar1 = new Bar('nueva era', 50);
 var Bar2 = new Bar('el patio', 200);
-console.log(Bar1);
-console.log(Bar2);
+Bar1.setNombre('estrella');
+Bar2.setCapacidad(70);
+console.log(Bar1.getNombre());
+console.log(Bar2.getCapacidad());
 var BarMan1 = new BarMan('sheila', 'Miranda');
 var BarMan2 = new BarMan('thomas', 'Patzi');
-console.log(BarMan1);
-console.log(BarMan2);
+BarMan1.setNombre('dota');
+BarMan2.setApellido('laime');
+console.log(BarMan1.getNombre());
+console.log(BarMan2.getApellido());
 var Trago1 = new Trago('botka', 'rosado', 60);
 var Trago2 = new Trago('tekila', 'blanco', 150);
-console.log(Trago1);
-console.log(Trago2);
+Trago1.setNombre('sampan');
+Trago2.setColor('verde');
+Trago2.setcantidad(150);
+console.log(Trago1.getNombre());
+console.log(Trago2.getColor());
+console.log(Trago2.getCantidad());
--- main.ts
+++ main.ts
@@ -1,41 +1,91 @@
 export class Bar {
-    nombre: string;
-    capacidad: number;
+    private nombre: string;
+    private capacidad: number;
     constructor(nom: string, cap: number) {
         this.nombre = nom;
         this.capacidad = cap;
     }
+    public setNombre(nom: string): void {
+        this.nombre = nom;
+    }
+    public setCapacidad(cap: number): void {
+        this.capacidad = cap;
+    }
+    public getNombre(): string {
+        return this.nombre;
+    }
+    public getCapacidad(): number {
+        return this.capacidad;
+    }
 }
 export class BarMan {
-    nombre: string;
-    apellido: string;
+    private nombre: string;
+    private apellido: string;
     constructor(nom: string, ape: string) {
         this.nombre = nom;
         this.apellido = ape;
     }
+    public setNombre(nom: string): void {
+        this.nombre = nom;
+    }
+    public setApellido(ape: string): void {
+        this.apellido = ape;
+    }
+    public getNombre(): string {
+        return this.nombre;
+    }
+    public getApellido(): string {
+        return this.apellido;
+    }
 }
+
 export class Trago {
-    nombre: string;
-    color: string;
-    cantidad: number;
+    private nombre: string;
+    private color: string;
+    private cantidad: number;
     constructor(nom: string, col: string, cant: number) {
         this.nombre = nom;
         this.color = col;
         this.cantidad = cant;
     }
+    public setNombre(nom: string): void {
+        this.nombre = nom;
+    }
+    public setColor(col: string): void {
+        this.color = col;
+    }
+    public setcantidad(cant: number): void {
+        this.cantidad = cant;
+    }
+    public getNombre(): string {
+        return this.nombre;
+    }
+    public getColor(): string {
+        return this.color;
+    }
+    public getCantidad(): number {
+        return this.cantidad;
+    }
 }
 let Bar1: Bar = new Bar('nueva era', 50);
 let Bar2: Bar = new Bar('el patio', 200);
-console.log(Bar1);
-console.log(Bar2);
+Bar1.setNombre('estrella');
+Bar2.setCapacidad(70);
+console.log(Bar1.getNombre());
+console.log(Bar2.getCapacidad());
 
 let BarMan1: BarMan = new BarMan('sheila', 'Miranda');
 let BarMan2: BarMan = new BarMan('thomas', 'Patzi');
-console.log(BarMan1);
-console.log(BarMan2);
+BarMan1.setNombre('dota');
+BarMan2.setApellido('laime');
+console.log(BarMan1.getNombre());
+console.log(BarMan2.getApellido());
 
 let Trago1: Trago = new Trago('botka', 'rosado', 60);
 let Trago2: Trago = new Trago('tekila', 'blanco', 150);
-console.log(Trago1);
-console.log(Trago2);
-
+Trago1.setNombre('sampan');
+Trago2.setColor('verde');
+Trago2.setcantidad(150);
+console.log(Trago1.getNombre());
+console.log(Trago2.getColor());
+console.log(Trago2.getCantidad());
